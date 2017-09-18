562aa8d11b501d765c63f22facad1e60ea80f2ff
Yash <purigr8@gmail.com>
2017-09-18 21:33:51 +1000

    Weather Information Widget

---
+++ README.md
@@ -0,0 +1,17 @@
+# Weather-Dashboard-Widget
+
+Wordpress plugin to show weather information for Brookvale , NSW , Australia on dashboard . Updates infromation every 45 minutes .
+
+### Installing
+
+Download and extract folder in your plugins directory in wordpress. And install plugin named Weather Widget .
+
+### Output
+
+You will be able to see weather information under Weather Dashboard Widget heading .
+
+## Authors
+
+* **Abstain Solutions** 
+
+### Website Url : - http://abstainsolutions.com/
---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Plugin Name: Weather Widget
+Description: Weather Widget
+Author: Abstain Solutions
+*/
+function weather_widget_main_function() 
+{
+	wp_add_dashboard_widget(
+    'weather_dashboard_widget',         // Widget slug.
+    'Weather Dashboard Widget',         // Title.
+    'weather_child_function' 			// Display function.
+    );	
+}
+add_action( 'wp_dashboard_setup', 'weather_widget_main_function' );
+/**
+ * Create the function to output the contents of our Dashboard Widget.
+ */
+function weather_child_function() 
+{
+	?>
+	<script>
+	function api()				// function to fetch data from api 
+	{
+		jQuery.ajax({
+		  type:"post",
+		  url:"http://api.openweathermap.org/data/2.5/weather?q=Brookvale,NSW,Australia&mode=html&appid=64e504230084c0d1eced463bed45bd40",
+		  datatype:"html",
+		  success:function(data)
+		  {
+			 jQuery('.widget_weather').html(data);		//added data in widget_weather class
+		  }
+		});
+	}
+	jQuery(document).ready(function(){
+	api();												
+	setInterval(function()									
+	{ 
+    api();
+	},45*60000);//time in milliseconds 45 minutes
+	}) 
+	</script>
+	<div class="widget_weather"></div>
+<?php
+}
+?>
